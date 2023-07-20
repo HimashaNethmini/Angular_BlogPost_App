a2ef1cb4fd55dc25ec41f230ed644d60f396ebda
HimashaNethmini <himashanethmini05@gmail.com>
2023-07-20 08:53:39 +0530

    completed components creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { SubscriptionFormComponent } from './subscription-form/subscription-form.component';
 import { CommentFormComponent } from './comments/comment-form/comment-form.component';
 import { CommentListComponent } from './comments/comment-list/comment-list.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { CommentListComponent } from './comments/comment-list/comment-list.compo
     ContactUsComponent,
     SubscriptionFormComponent,
     CommentFormComponent,
-    CommentListComponent
+    CommentListComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
